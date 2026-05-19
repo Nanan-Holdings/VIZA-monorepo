37befc9298b0a26fc38f70440331aba06bf7412e
Chen Hongyu <e1484122@u.nus.edu>
2026-05-19 11:02:35 +0800

    Fix status

--- viza-fe/internal-website/app/client/documents/page.tsx
+++ viza-fe/internal-website/app/client/documents/page.tsx
@@ -13,7 +13,6 @@ export default function DocumentsPage() {
       applicationId={searchParams.get("applicationId")}
       country={searchParams.get("country")}
       visaType={searchParams.get("visaType")}
-      startedOnly
       basePath="/client/documents"
     />
   );
