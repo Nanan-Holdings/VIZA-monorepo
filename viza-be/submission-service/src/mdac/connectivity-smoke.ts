41ec7ed6af5148f64d35086e1903d5bffa908552
Chen Hongyu <e1484122@u.nus.edu>
2026-07-15 11:26:28 +0800

    fix(mdac): package connectivity smoke in worker image

--- viza-be/submission-service/src/mdac/connectivity-smoke.ts
+++ viza-be/submission-service/src/mdac/connectivity-smoke.ts
@@ -1,9 +1,8 @@
-#!/usr/bin/env npx tsx
 import * as fs from "node:fs";
 import * as os from "node:os";
 import * as path from "node:path";
-import { createArrivalCardBrowserSession } from "../src/arrival-card-browser";
-import { MDAC_OFFICIAL_PORTAL_URL } from "../src/mdac/normalize";
+import { createArrivalCardBrowserSession } from "../arrival-card-browser";
+import { MDAC_OFFICIAL_PORTAL_URL } from "./normalize";
 
 async function main(): Promise<void> {
   const session = await createArrivalCardBrowserSession({ prefix: "MDAC", headless: true });
